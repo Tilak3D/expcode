b56bff2ecc988b74c1b32e55f7669a83a4db4fac
Tilak3D <109524845+Tilak3D@users.noreply.github.com>
2022-07-26 00:58:42 +0530

    revert

--- js/analytics.js
+++ js/analytics.js
@@ -1,14 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X5ZRSNWVNG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'G-X5ZRSNWVNG']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-function recordOutboundLink(link, category, action) {
-_gat._getTrackerByName()._trackEvent(category, action);
-setTimeout('document.location = "' + link.href + '"', 100);
-}
+  gtag('config', 'G-X5ZRSNWVNG');
+</script>
